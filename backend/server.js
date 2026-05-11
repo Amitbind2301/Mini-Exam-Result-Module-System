31d500d21c656ba3bc605ad510c8f424e28d0a56
Amit bind <amitbind1747@gmail.com>
2026-05-12 03:46:43 +0530

    updatedb

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://your-frontend.vercel.app",
+  origin: "https://mini-exam-result-module-system.vercel.app/",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
